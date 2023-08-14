837179a7f0538ffb4ed41d5e8ee46e570d33abb2
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-08-14 11:27:39 +0530

    backend: Update openai chat completion method

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Request
 import openai
+import os
 
 app = FastAPI()
 openai.api_key = ''
@@ -14,22 +15,20 @@ async def get_code(request: Request):
 
     architecture_explanation = "EC2 instance in a private subnet of a VPC"
     cloud_provider = "AWS"
+    
+    prompt=generate_prompt(architecture_explanation, cloud_provider),
 
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo",
-        prompt=generate_prompt(architecture_explanation, cloud_provider),
-        temperature=0.6
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k-0613",
+    messages=[
+            {"role": "user", "content": "Generate Terraform code for EC2 instance in a private subnet of a VPC in AWS    cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files"},
+        ]
     )
 
-    print(response)
-
-    return {"message": "Success"}
-
+    return {"message": response}
 
 def generate_prompt(architecture_explanation: str, cloud_provider: str):
-    return """Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files.
-    """.format(architecture_explanation, cloud_provider)
-
+    return """Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files""".format(architecture_explanation, cloud_provider)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="localhost", port=8080, reload=True)
