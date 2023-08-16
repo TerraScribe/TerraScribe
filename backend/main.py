3151b2400f726907db7401f50f36d8cb406d6cbb
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-08-16 15:42:19 +0530

    backend: Add dotenv to requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI, Request
 import openai
+from dotenv import load_dotenv
 import os
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 from pydantic import BaseModel
 
 load_dotenv() 
@@ -11,7 +11,8 @@ load_dotenv()
 
 class Prompt(BaseModel):
     prompt: str
-    
+
+
 app = FastAPI()
 origins = ["*"]
 
@@ -50,20 +51,22 @@ async def get_code(request: Request):
     architecture_explanation = "EC2 instance in a private subnet of a VPC"
     cloud_provider = "AWS"
 
-    prompt = generate_prompt(architecture_explanation, cloud_provider),
+    prompt = "Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files".format(architecture_explanation, cloud_provider)
+
 
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo-16k-0613",
         messages=[
-            {"role": "user", "content": "Generate Terraform code for EC2 instance in a private subnet of a VPC in AWS    cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files"},
+            {
+                "role": "user", 
+                "content": prompt
+            }
         ]
     )
-    print('here')
-
-    return {"message": response}
-
-def generate_prompt(architecture_explanation: str, cloud_provider: str):
-    return """Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files. Provide the output format as json such that the terraform tf file is key and the terraform code as value""".format(architecture_explanation, cloud_provider)
+    return {
+        "model": response['model'],
+        "content": response['choices'][0]['message']['content']
+    }
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="localhost", port=8080, reload=True)
