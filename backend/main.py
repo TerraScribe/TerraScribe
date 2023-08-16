9ca96af8298e0a540247a74ccbe6e88c028bb4c9
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2023-08-16 14:49:18 +0530

    backend: Update visualizer api

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,14 @@ import openai
 import os
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv() 
 
+
+class Prompt(BaseModel):
+    prompt: str
+    
 app = FastAPI()
 origins = ["*"]
 
@@ -24,19 +29,20 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/visualize")
-def visualize(request: Request):
-    
+@app.post("/visualize")
+async def visualize(prompt: Prompt):
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-16k-0613",
     messages=[
-            {"role": "user", "content": "Generate a json code to visualize the architecture to provision an ec2 instance within a vpc connect with rds and s3 bucket"},
+            {"role": "user", "content": "Generate a json code to visualize the architecture to "+prompt.prompt},
         ]
     )
     response = response["choices"][0]["message"]["content"]
     if "```json" in response:
         response = response.split("```json",1)[1]
         response = response.split("```",1)[0]
+
+    print("visualized", response)
         
     return {"message": response} 
 
