190a083d8f948cf50fd6a539f434e890d71c8a56
antoprince001 <antoprince001@gmail.com>
2023-08-21 12:19:58 +0530

    Update analysis component

--- backend/apis/code.py
+++ backend/apis/code.py
@@ -33,7 +33,18 @@ async def get_code(request: Request):
 @router.post("/visual")
 async def get_code_using_json(prompt: PromptVisual):
     cloud_provider = 'aws'
-    prompt = "Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files. Use the terraform requirement in the form of json {} to generate the code ".format(prompt.prompt, cloud_provider, json.dumps(prompt.visual_json))
+    # prompt = "Generate Terraform code for {} in {} cloud. Please provide the Terraform code with appropriate resource names, variable definitions, module organization and group and separate it into multiple tf files. Use the terraform requirement in the form of json {} to generate the code ".format(prompt.prompt, cloud_provider, json.dumps(prompt.visual_json))
+    prompt = "Generate Terraform code for provisioning a {} in {} cloud. The architecture involves: \
+    - <List key components and services needed for the architecture>.\
+    - The setup should follow best practices for security, scalability, and maintainability.\
+    - Implement the principle of least privilege for IAM roles and permissions.\
+    - Use variables for customizable configuration options.\
+    - Organize the code into separate `.tf` files for improved modularity and readability.\
+    - Document each resource and module for future reference.\
+    - Ensure the architecture has strategy for backup and Disaster Recovery.\
+    - Enable proper monitoring and logging for the infrastructure.\
+    Provide the Terraform code in the output format given below that accomplishes the above requirements, including appropriate resource names, variable definitions, and module organization. \
+    Use the terraform requirement in the form of json {} to generate the code ".format(prompt.prompt, cloud_provider, json.dumps(prompt.visual_json))
     model = "gpt-3.5-turbo-16k-0613"
 
     response = await openai_call(model, prompt)
@@ -43,7 +54,7 @@ async def get_code_using_json(prompt: PromptVisual):
 
 @router.post("/analyze")
 async def analyze_code(code: CodeRequest):
-    prompt = "Provide analysis, code reviewm security vulnerability scanning and optimization strategies for the following terraform code and return the output as bullet points within 300 words, code : {}".format(code.code)
+    prompt = "Provide analysis, code review security vulnerability scanning and optimization strategies for the following terraform code and return the output where each section is split using numbers within 300 words, code : {}".format(code.code)
     model = "gpt-3.5-turbo-16k-0613"
 
     response = await openai_call(model, prompt)
--- frontend/components/CodeAnalysis.tsx
+++ frontend/components/CodeAnalysis.tsx
@@ -7,11 +7,16 @@ interface CodeAnalysisProps {
 }
 
 const CodeAnalysis: FC<CodeAnalysisProps> = ({ analysis }) => {
+
     return (
         <div className="flex justify-between">
             <div className="w-[100%] mx-5">
                 <div className="text-lg text-center text-gray-800">
-                    {analysis}
+                    <ul className="space-y-2">
+                        {analysis.split('\n').map((part, index) => (
+                        <li key={index} className="bg-white p-3 shadow-md rounded">{part}</li>
+                        ))}
+                    </ul>
                 </div>        
             </div>
         </div>
